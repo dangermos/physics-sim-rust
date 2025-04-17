f85f54d62ed89eda461e69d725b7a9bbd32ab66f
Danny <dannygermosen123@gmail.com>
2025-04-17 13:50:03 -0400

    Implemented a basic collision system between two particles in the second dimension.
    Particles do not yet calculate angle of movement, and only simulate collisions naively by reversing the velocity in both x and y, which is not only wrong but also only assumes completely elastic collisions.

--- particles/src/lib.rs
+++ particles/src/lib.rs
@@ -1,10 +1,29 @@
+use core::f32;
+use std::f32::consts::PI;
 
+use macroquad::math::bool;
 
-pub struct Particle2D 
+/// An object that simulates a circular particle in space under the laws of kinematics and momentum
+///
+///
+///
+pub struct Particle2D
+
 {
-pub pos: Vec2,
-velocity: Vec2,
-acceleration: Vec2,
+    // Movement
+    pub pos: Vec2,
+    velocity: Vec2,
+    acceleration: Vec2,
+    angle_of_movement: f32, // Degrees
+
+
+    // Properties
+    radius: f32,
+    collided: bool,
+
+    // Pre-Computed Properties
+    area: f32,
+
 }
 
 pub struct Vec2 {
@@ -12,23 +31,66 @@ pub struct Vec2 {
     pub y: f32,
 }
 
+// TODO
+// Add delta time as a calculation for the kinematics equations
+
 
 impl Particle2D {
 
-    pub fn new(pos: Vec2, velocity: Vec2, acceleration: Vec2) -> Particle2D {
+    pub fn new(pos: Vec2, velocity: Vec2, acceleration: Vec2, radius: f32) -> Particle2D {
         
-        Self {pos, velocity, acceleration}
+        Self {pos,
+            velocity,
+            acceleration,
+            radius,
+            angle_of_movement: 0.0,
+            collided: false,
+            area: radius.powf(2.0) * PI,
+        }
 
+    }
+
+    pub fn get_radius(&self) -> f32{
+        self.radius
     }
 
     pub fn set_velocity(&mut self, velocity: Vec2) {
         self.velocity = velocity;
     }
 
+    pub fn is_collided_other(&mut self, other: &mut Particle2D) -> bool {
+        let dist_x: f32 = self.pos.x - other.pos.x;
+        let dist_y : f32 = self.pos.y - other.pos.y;
+        let distance = ( dist_x.powf(2.0) + dist_y.powf(2.0) ).sqrt();
+        let radii_sum = self.radius + other.radius;
+        distance < radii_sum
 
-    pub fn new_newtonian(pos: Vec2) -> Particle2D {
+    }
 
-        Self { pos, velocity: Vec2 { x: 0.0, y: 0.0 }, acceleration: Vec2 { x: 0.0, y: 9.81 } }
+    pub fn handle_collision_from_other_particle(&mut self, other: &mut Particle2D) {
+
+        // if not within some range, just pass the function
+        // as this can be called every frame
+        let range: f32 = 1.0; // TODO
+
+        self.collided = true;
+        self.collision();
+
+        other.collided = true;
+        other.collision();
+
+    }
+
+    pub fn new_newtonian(pos: Vec2, radius: f32) -> Particle2D {
+
+        Self { pos,
+            velocity: Vec2 { x: 0.0, y: 0.0 },
+            acceleration: Vec2 { x: 0.0, y: 9.81 },
+            radius,
+            angle_of_movement: 270.0,
+            collided: false,
+            area: radius.powf(2.0) * PI,
+        }
 
     }
 
